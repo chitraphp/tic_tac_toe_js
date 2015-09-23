ba173fe9cf3ff0e47a212fd8469a405e8e808eb6
chitra atmakuru & Ashley Sullins <student@epicodus.com>
2015-09-23 12:22:31 -0700

    Add passing test for board

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,37 @@ Player.prototype.mark = function() {
   return this.playerMark;
 }
 
+function Space(xcoordinate, ycoordinate) {
+  this.xcoordinate = xcoordinate;
+  this.ycoordinate = ycoordinate;
+}
 
+Space.prototype.mark_by = function(playerMark) {
+  return this.playerMark = playerMark;
+}
+
+Space.prototype.markedBy = function() {
+  return this.playerMark;
+}
+
+function numberOfSpaces() {
+  var array = [];
+
+  for (var x = 1;x<=3;x++){
+    for(var y=1;y<=3;y++)
+  { array.push(new Space(x,y));
+    }
+  }
+  return array;
+}
+
+function Board(numberOfSpaces) {
+  this.numberOfSpaces = numberOfSpaces;
+}
+
+  Board.prototype.spaces = function() {
+    return this.numberOfSpaces = this.numberOfSpaces.length;
+  }
 //
 // $(document).ready(function() {
 //   $("form#factorial").submit(function(event) {
--- spec/specs.js
+++ spec/specs.js
@@ -4,3 +4,30 @@ describe('Player', function() {
       expect(testPlayer.mark()).to.equal("X");
    });
 });
+
+describe('Space', function() {
+    it("returns the player's mark", function() {
+      var testSpace = new Space(1,2);
+      expect(testSpace.xcoordinate).to.equal(1);
+   });
+
+   it("returns the player's mark", function() {
+     var testSpace = new Space(1,2);
+     expect(testSpace.ycoordinate).to.equal(2);
+  });
+
+  it("lets a player mark a space", function() {
+    var testPlayer = new Player("X");
+    var testSpace = new Space(1,2);
+    testSpace.mark_by(testPlayer);
+    expect(testSpace.markedBy()).to.equal(testPlayer);
+ });
+});
+
+describe('Board', function() {
+  it("creates 9 spaces when it is initialized", function() {
+    var newSpaces = new numberOfSpaces();
+    var newBoard = new Board(newSpaces);
+    expect(newBoard.spaces()).to.equal(9);
+   });
+});
